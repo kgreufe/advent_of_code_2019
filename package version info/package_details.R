33fce474a508a5ed6e600e6d0c77718a401f9fe5
kgreufe <kgreufe@gmail.com>
2021-10-19 18:04:42 -0600

    add package version info

---
+++ package version info/package_details.R
@@ -0,0 +1,14 @@
+# These were solved using R version 4.1.0 and the following Tidyverse package versions:
+
+# |package   |loadedversion |
+# |:---------|:-------------|
+# |dplyr     |1.0.7         |
+# |forcats   |0.5.1         |
+# |ggplot2   |3.3.5         |
+# |purrr     |0.3.4         |
+# |readr     |2.0.0         |
+# |stringr   |1.4.0         |
+# |tibble    |3.1.3         |
+# |tidyr     |1.1.3         |
+# |tidyverse |1.3.1         |
+
